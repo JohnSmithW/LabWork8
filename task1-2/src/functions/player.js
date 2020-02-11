f4088f87b00eb35ccbcbc2c1a8c0957b938f4d81
JohnSmithW <55023917+JohnSmithW@users.noreply.github.com>
2020-02-11 16:24:29 +0300

    fixed some issues

--- task1-2/src/functions/player.js
+++ task1-2/src/functions/player.js
@@ -4,29 +4,33 @@ function Player(tracklist) {
   this.trackList = tracklist;
   this.currentTrack = 0;
   this.status = 'pause';
-  this.display = function() {
-      if (this.trackList.length > 0) {
-        return 'Track: ' + this.trackList[this.currentTrack] + ' Status: ' + this.status;
-      } else { return 'track you are looking for is not found'; }
-    },
-    this.play = function() {
-      this.status = 'play';
-    },
-    this.pause = function() {
-      this.status = 'pause';
-    },
-    this.next = function() {
-      this.currentTrack = (this.currentTrack + 1);
-      if (this.currentTrack > this.trackList.length - 1) {
-        this.currentTrack = 0;
-      }
-    },
-    this.prev = function() {
-      this.currentTrack = (this.currentTrack - 1);
-      if (this.currentTrack < 0 && this.trackList.length > 1) {
-        this.currentTrack = this.trackList.length - 1;
-      } else { this.currentTrack = 0; }
-    };
+  this.display = function () {
+    if (this.trackList.length > 0) {
+      return 'Track: ' + this.trackList[this.currentTrack] + ' Status: ' + this.status;
+    } else {
+      return 'track you are looking for is not found';
+    }
+  };
+  this.play = function () {
+    this.status = 'play';
+  };
+  this.pause = function () {
+    this.status = 'pause';
+  };
+  this.next = function () {
+    this.currentTrack = (this.currentTrack + 1);
+    if (this.currentTrack > this.trackList.length - 1) {
+      this.currentTrack = 0;
+    }
+  };
+  this.prev = function () {
+    this.currentTrack = (this.currentTrack - 1);
+    if (this.currentTrack < 0 && this.trackList.length > 1) {
+      this.currentTrack = this.trackList.length - 1;
+    } else {
+      this.currentTrack = 0;
+    }
+  };
 }
 
 
